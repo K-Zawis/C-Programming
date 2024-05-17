a6cb9e7efdbb14469dbf705c5cfe056cf1d73189
Kasia Zawistowska <zawistowska.kasia@outlook.com>
2024-05-17 22:31:30 +0100

    feat(*) Chapter 2 complete. Ez

---
+++ DataTypeExample/mian.c
@@ -0,0 +1,10 @@
+/* A Program that Uses the Characters, Integers, and Floaing-Point Data Types */
+#include <stdio.h>
+
+int main()
+{
+    printf("I am learning the %c programming language\n", 'C');
+    printf("I have just completed Chapter %d\n", 2);
+    printf("I am %.1f percent ready to move on to the next chapter", 99.9);
+    return 0;
+}
